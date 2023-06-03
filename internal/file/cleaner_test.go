3da4c545d6cb26a428f3e2391391f93bc0cfbd26
Jun Nishimura <n.junjun0303@gmail.com>
2023-06-03 19:55:41 +0900

    add TestExecute (#9)

--- internal/file/cleaner_test.go
+++ internal/file/cleaner_test.go
@@ -1,6 +1,9 @@
 package file
 
 import (
+	"errors"
+	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 
@@ -39,6 +42,69 @@ func TestNewCleaner(t *testing.T) {
 			got := NewCleaner(tt.args.filePath)
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("got = %v, want = %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestExecute(t *testing.T) {
+	tests := []struct {
+		name    string
+		content string
+		want    string
+		wantErr error
+	}{
+		{
+			name: "success",
+			content: `
+package main
+// import package
+import "fmt"
+/*
+comment
+*/
+func main() {
+	fmt.Println("Hello, Wolrd")// print out hello world
+}`,
+			want: `
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Wolrd")
+}`,
+			wantErr: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tmpDir := t.TempDir()
+			testPath := filepath.Join(tmpDir, "test.go")
+			f, err := os.Create(testPath)
+			if err != nil {
+				t.Log(err)
+			}
+
+			_, err = f.WriteString(tt.content)
+			if err != nil {
+				t.Log(err)
+			}
+			f.Close()
+
+			cleaner := NewCleaner(testPath)
+			err = cleaner.Execute()
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("got = %v, want = %v", err, tt.wantErr)
+			}
+
+			testCleanedPath := filepath.Join(tmpDir, "test_cleaned.go")
+			b, err := os.ReadFile(testCleanedPath)
+			if err != nil {
+				t.Log(err)
+			}
+			if string(b) != tt.want {
+				t.Errorf("got = %s, want = %s", string(b), tt.want)
 			}
 		})
 	}
