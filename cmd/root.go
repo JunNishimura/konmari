218311a5357136aa6d68ca3466f63bf9f60f8f2c
Jun Nishimura <n.junjun0303@gmail.com>
2023-06-03 12:11:23 +0900

    add file existence check (#7)

--- cmd/root.go
+++ cmd/root.go
@@ -22,26 +22,27 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "konmari",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "clean up source code comments",
+	Long:  "clean up source code comments",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// check file existence
+		for _, arg := range args {
+			if _, err := os.Stat(arg); os.IsNotExist(err) {
+				return fmt.Errorf("cannot find '%s'", arg)
+			}
+		}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+		return nil
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -54,15 +55,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.konmari.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
