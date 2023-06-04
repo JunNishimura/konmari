de10b1f7afdfd5f10b687db53da6c4f742897a27
Jun Nishimura <n.junjun0303@gmail.com>
2023-06-04 12:45:10 +0900

    add testdata (#33)

---
+++ testdata/main.swift
@@ -0,0 +1,16 @@
+import UIKit
+// one line comment
+/*
+multiple
+line
+comment
+*/
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        println("HelloWorld!") // comment
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
