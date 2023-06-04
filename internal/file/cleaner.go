a626be986a5b4e968ce5cab9710e12b257bca922
Jun Nishimura <n.junjun0303@gmail.com>
2023-06-04 12:59:40 +0900

    clean kotlin file (#35)

--- internal/file/cleaner.go
+++ internal/file/cleaner.go
@@ -27,6 +27,7 @@ var (
 		extension.Cpp:        `\/\*[\s\S]*?\*\/|\/\/.*`,
 		extension.Rust:       `\/\*[\s\S]*?\*\/|\/\/.*`,
 		extension.Swift:      `\/\*[\s\S]*?\*\/|\/\/.*`,
+		extension.Kotlin:     `\/\*[\s\S]*?\*\/|\/\/.*`,
 	}
 )
 
